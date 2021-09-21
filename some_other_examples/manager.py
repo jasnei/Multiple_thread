37bd949014768add3e3430c896b87222f7c4a103
albertyang <albertyang@soundpie.cn>
2021-09-21 14:17:01 +0800

    manager

---
+++ some_other_examples/manager.py
@@ -0,0 +1,23 @@
+# 进程间数据是独立的，可以借助于队列或管道实现通信，二者都是基于消息传递的
+# 虽然进程间数据独立，但可以通过Manager实现数据共享，事实上Manager的功能远不止于此
+from multiprocessing import Manager, Process, Lock
+
+
+def work(d, lock):
+    with lock: #不加锁而操作共享的数据,肯定会出现数据错乱
+        d['count'] -= 1
+
+
+if __name__ == '__main__':
+    lock = Lock()
+    with Manager() as m:
+        dic = m.dict({'count': 100})
+        p_l = []
+        for i in range(100):
+            p = Process(target=work, args=(dic, lock))
+            p_l.append(p)
+            p.start()
+        for p in p_l:
+            p.join()
+        print(dic)
+        #{'count': 94}
