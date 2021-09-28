867349259bebc733b17557c8b38dac1f85c8dbd5
albertyang <albertyang@soundpie.cn>
2021-09-28 10:57:09 +0800

    read image multiple

---
+++ some_other_examples/read_images_loader.py
@@ -0,0 +1,102 @@
+from multiprocessing import Process, Queue
+import time
+import os
+import numpy as np
+from PIL import Image
+import threading
+
+
+def producer(file_name):
+    img = Image.open(file_name)
+    new_img = img.resize((480, 480))
+    # fn = "level_0_" + str(i)
+    # fn = r"some_other_examples\data" + "/" + fn + ".png"
+    # image = np.random.randint(0, 255, (1024, 1024, 3), dtype=np.uint8)
+    # q.put([fn, image])
+    # print(f"Open {file_name}")
+
+
+
+def producer_1(q):
+    for i in range(500):
+        fn = "level_0_" + str(i)
+        fn = r"some_other_examples\data" + "/" + fn + ".png"
+        image = np.random.randint(0, 255, (1024, 1024, 3), dtype=np.uint8)
+        q.put([fn, image])
+        print(f"Generate {fn}")
+
+
+def consumer(q):
+    while True:
+        res = q.get()
+        if res is None:
+            break  # End signal
+        fn, image = res[0], res[1]
+        save(fn, image)
+
+
+def save(fn, image):
+    tile = Image.fromarray(image)
+    tile.save(fn, format="png", quality=100)
+    print(f"{fn} saved")
+
+
+def main():
+    q = Queue(maxsize=500)
+
+    # Now one 1 producer is most fast than others
+    produce_list = []
+    for i in range(1):
+        p1 = Process(target=producer, args=(q,))
+        # can be changed to threading.thead
+        # p1 = threading.Thread(target=producer, args=(q,))
+        p1.start()
+        produce_list.append(p1)
+
+    consum_list = []
+    for i in range(10):
+        c1 = Process(target=consumer, args=(q,))
+        # c1 = threading.Thread(target=consumer, args=(q,))
+        c1.start()
+        consum_list.append(c1)
+
+    for p in produce_list:
+        p.join()
+
+    for i in range(len(consum_list)):
+        q.put(None)
+
+
+if __name__ == "__main__":
+    from concurrent.futures import ProcessPoolExecutor
+    from multiprocessing import Pool
+    from utils import  get_specified_files
+    path = r"F:\Work\Data\svs_ano\test_split"
+    files = get_specified_files(path, suffixes=[".png", ".jpg"], recursive=True)
+    print(len(files))
+
+    start_time = time.time()
+    #=======================================================
+    # Read 16007 image in 3.49s, resize 480 76.59s
+    #=======================================================
+    for file in files[:]:
+        img = Image.open(file)
+        new_img = img.resize((480, 480))
+
+    #=======================================================
+    # Read 16007 image in 11.62s, resize 480 76.59s
+    #=======================================================
+    # p = ProcessPoolExecutor(max_workers=4)
+    # results = p.map(producer, files)
+    # for result in results:
+    #     pass
+
+    #=================Not Working=================
+    # produce_list = []
+    # for i in range(2):
+    #     p1 = Process(target=producer, args=(files[1],))
+    #     p1.start()
+    #     produce_list.append(p1)
+
+    end_time = time.time()
+    print(f"Elapse: {end_time - start_time}")
