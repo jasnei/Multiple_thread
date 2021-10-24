d39c7f9c509c6bd284ab77dfc724db7b02234d44
albertyang <albertyang@soundpie.cn>
2021-10-24 15:52:54 +0800

    lockcounter

---
+++ effective_python/lock.py
@@ -0,0 +1,32 @@
+from threading import Thread, Lock
+
+
+class Counter():
+    def __init__(self):
+        self.lock = Lock()
+        self.count = 0
+
+    def increment(self, offset):
+        with self.lock:
+            self.count += offset
+
+
+def worker(sensor_index, how_many, counter):
+    for _ in range(how_many):
+        counter.increment(1)
+
+how_many = 10**5
+counter = Counter()
+
+threads = []
+for i in range(5):
+    thread = Thread(target=worker, args=(i, how_many, counter))
+    threads.append(thread)
+    thread.start()
+
+for thread in threads:
+    thread.join()
+
+expected  = how_many * 5
+found = counter.count
+print(f"Counter should be {expected:,}, got {found:,}")
