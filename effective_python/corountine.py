84cab2795409453613f3496b1d526536c9d269bb
albertyang <albertyang@soundpie.cn>
2021-11-01 11:27:30 +0800

    asyncio

---
+++ effective_python/corountine.py
@@ -0,0 +1,125 @@
+import time
+import asyncio
+
+
+ALIVE = '*'
+EMPTY = '-'
+
+class Grid:
+    def __init__(self, height, width):
+        self.height = height
+        self.width = width
+        self.rows = []
+        for _ in range(self.height):
+            self.rows.append([EMPTY] * self.width)
+
+    def get(self, y, x):
+        return self.rows[y % self.height][x % self.width]
+
+    def set(self, y, x, state):
+        self.rows[y % self.height][x % self.width] = state
+
+    def __str__(self):
+        output = ''
+        for row in self.rows:
+            for cell in row:
+                output += cell
+            output += '\n'
+        return output
+
+
+def count_neighbors(y, x, get):
+    n_ = get(y - 1, x + 0)
+    ne = get(y - 1, x + 1)
+    e_ = get(y + 0, x + 1)
+    se = get(y + 1, x + 1)
+    s_ = get(y + 1, x + 0)
+    sw = get(y + 1, x - 1)
+    w_ = get(y + 0, x - 1)
+    nw = get(y - 1, x - 1)
+    neighbor_states = [n_, ne, e_, se, s_, sw, w_, nw]
+    count = 0
+    for state in neighbor_states:
+        if state == ALIVE:
+            count += 1
+    return count
+
+async def game_logic(state, neighbors):
+    if state == ALIVE:
+        if neighbors < 2:
+            return EMPTY     # Die: Too few
+        elif neighbors > 3:
+            return EMPTY     # Die: Too many
+    else:
+        if neighbors == 3:
+            return ALIVE     # Regenerate
+    return state
+
+async def step_cell(y, x, get, set):
+    state = get(y, x)
+    neighbors = count_neighbors(y, x, get)
+    next_state = await game_logic(state, neighbors)
+    set(y, x, next_state)
+
+#===========================================================================
+# asyncio
+#===========================================================================
+
+async def simulate_async(grid):
+    next_grid = Grid(grid.height, grid.width)
+
+    tasks = []
+    for y in range(grid.height):
+        for x in range(grid.width):
+            task = step_cell(y, x, grid.get, next_grid.set)  # Fan out
+            tasks.append(task)
+    
+    await asyncio.gather(*tasks)                             # Fan in
+
+    return next_grid
+
+class ColumnPrinter:
+    def __init__(self):
+        self.columns = []
+
+    def append(self, data):
+        self.columns.append(data)
+
+    def __str__(self):
+        row_count = 1
+        for data in self.columns:
+            row_count = max(
+                row_count, len(data.splitlines()) + 1)
+
+        rows = [''] * row_count
+        for j in range(row_count):
+            for i, data in enumerate(self.columns):
+                line = data.splitlines()[max(0, j - 1)]
+                if j == 0:
+                    padding = ' ' * (len(line) // 2)
+                    rows[j] += padding + str(i) + padding
+                else:
+                    rows[j] += line
+
+                if (i + 1) < len(self.columns):
+                    rows[j] += ' | '
+
+        return '\n'.join(rows)
+
+grid = Grid(5, 9)
+grid.set(0, 3, ALIVE)
+grid.set(1, 4, ALIVE)
+grid.set(2, 2, ALIVE)
+grid.set(2, 3, ALIVE)
+grid.set(2, 4, ALIVE)
+# print(grid)
+
+columns = ColumnPrinter()
+
+start = time.time()
+for i in range(5):
+    columns.append(str(grid))
+    grid = asyncio.run(simulate_async(grid))
+end = time.time()
+# print(columns)
+print(f"Elaps: {end - start:.4} seconds")
